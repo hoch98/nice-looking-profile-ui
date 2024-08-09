6fad9302acf22fb90d4e817eb690c292aa1c7bff
four <47018858+hoch98@users.noreply.github.com>
2024-08-09 22:27:30 +0800

    changed output type to use with github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: { domains: ['*.googleusercontent.com'], formats: ['image/avif', 'image/webp'], },
+  output: "export",
   experimental: {
     missingSuspenseWithCSRBailout: false,
   }
